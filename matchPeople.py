ba008a5637d235eb4a9122e0bab1e348a62c73be
Tomo <tprusina@mathos.hr>
2025-01-14 16:10:59 +0100

    Add argparse arguments to make running script easier

--- matchPeople.py
+++ matchPeople.py
@@ -77,6 +77,15 @@ def OKS(gt: np.ndarray, pp: np.ndarray, s: float = 1000, vi: np.ndarray = None, 
 
 
 def assignPeople(gts: np.ndarray, pps: np.ndarray, vi: np.ndarray, si: np.ndarray, **kwargs)-> tuple[np.ndarray,np.ndarray,np.ndarray]:
+    '''
+       gts - ground truth keypoints PxNx2
+       pps - predicted keypoints    RxNx2
+       vi - visibility of keypoints PxN
+       si - huma scale needed for function OKS Px1
+       
+       returns indexes and OKS values of matches (gt_indexs, pp_indexs, oks) in descending order of the match, i.e.
+       gt_indexs[i] and pp_indexs[i] are matched ground truth to prediction with OKS value of oks[i].
+    '''
     if gts.size == 0 or pps.size == 0:
         return (np.array([]), np.array([]), np.array([]))
     costs = OKS(gts[:,None,...], pps[None,:,...], vi=vi[:,None,:], s=si[:,None,None])
@@ -85,45 +94,6 @@ def assignPeople(gts: np.ndarray, pps: np.ndarray, vi: np.ndarray, si: np.ndarra
     row_ind = row_ind[indx]
     col_ind = col_ind[indx]
     return (row_ind, col_ind, costs[row_ind, col_ind])
-
-def assignPeopleGreedy(gts: np.ndarray, pps: np.ndarray, vi: np.ndarray, si: np.ndarray, **kwargs)-> tuple[np.ndarray,np.ndarray,np.ndarray]:
-    if gts.size == 0 or pps.size == 0:
-        return (np.array([]), np.array([]), np.array([]))
-    costs = OKS(gts[:,None,...], pps[None,:,...], vi=vi[:,None,:], s=si[:,None,None])
-    row_ind = []
-    col_ind = []
-    rows = [-1] * costs.shape[0]
-    cols = [-1] * costs.shape[1]
-    for ij in np.argsort(-costs.ravel()):
-        (i, j) = np.unravel_index(ij, costs.shape)
-        if rows[i] == -1 and cols[j] == -1:
-            rows[i] = j
-            cols[j] = i
-            row_ind.append(i)
-            col_ind.append(j)
-    
-    row_ind = np.array(row_ind)
-    col_ind = np.array(col_ind)
-    return (row_ind, col_ind, costs[row_ind, col_ind])
-
-def assignPeopleL2(gts: np.ndarray, pps: np.ndarray, vi: np.ndarray, **kwargs)-> tuple[np.ndarray,np.ndarray,np.ndarray]:
-    if gts.size == 0 or pps.size == 0:
-        return (np.array([]), np.array([]), np.array([]))
-    costs = L2(gts[:,None,...], pps[None,:,...], vi[:,None,:])
-    costs_og = costs.copy()
-    row_ind = []
-    col_ind = []
-    while True:
-        (i, j) = np.unravel_index(np.argmin(costs), costs.shape)
-        if costs[i,j] == np.inf: break
-        row_ind.append(i)
-        col_ind.append(j)
-        costs[i,:] = np.inf
-        costs[:,j] = np.inf
-    row_ind = np.array(row_ind)
-    col_ind = np.array(col_ind)
-    return (row_ind, col_ind, costs_og[row_ind, col_ind])
-
     
 if __name__ == '__main__':
     import json
@@ -133,32 +103,17 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose',
-                        action='store_true')
-    parser.add_argument('-s', '--save',
-                        action='store_true')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print progress.')
+    parser.add_argument('-path', type=str, required=True, help='Path to the folder of results. The structure of the folder has to be similar to the paper one.')
+    parser.add_argument('-datasets', type=str, nargs='*', help='Names of datasets/folders to process. If left blank it will process all datasest in the given -path directory.')
+    parser.add_argument('-methods', type=str, nargs='*', help='Names of methods/folders to process. If left blank it will process all methods in the given path/dataset directory.')
+    parser.add_argument('-out', type=str, help='File to save the results. If left blank then the results will not be saved.')
     
-
     args = parser.parse_args()
     
-    pth = '/mnt/hdd/datasets/IoTGym/panoptic/'
-    datasets = [
-        '160906_band2',
-        '160906_ian1',
-        '160906_pizza1',
-        '170221_haggling_m3',
-        '170307_dance5',
-        '170915_office1',
-        '171026_pose3'
-        ]
-
-    methods = [
-        'openpose',
-        'rtmo',
-        'rtmpose-l',
-        'yolo11x',
-        'sapiens'
-        ]
+    pth = args.path
+    datasets = [ x for x in os.listdir(pth) if os.path.isdir(os.path.join(pth, x)) and x != 'results' ] if args.datasets is None else args.datasets
+    methods = [ x for x in os.listdir(os.path.join(pth, 'results')) if os.path.isdir(os.path.join(pth, 'results', x)) ] if args.methods is None else args.methods
     
     regx = re.compile(r'hdPose2D_(.*)\.json')
     
@@ -184,8 +139,6 @@ if __name__ == '__main__':
                 fl_path = os.path.join(dat_path, fl)
                 with open(fl_path) as fl_json:
                     data_gt = json.load(fl_json)
-
-                #plot_data = { method: {'scores':[], 'L2': []} for method in methods }
 
                 for method in methods:
                     meth_path = os.path.join(pth, 'results', method, f'{method}_{dat}_hd_{fl_name}.json')
@@ -231,15 +184,7 @@ if __name__ == '__main__':
 
                         try:
      
-                            row_gri, col_gri, costs_OKS = assignPeople(keypoints_gt, keypoints_dt, visibility_gt, bboxs_gt)
-                            #if row_gri.size > 0:
-                            #    dists = np.linalg.norm(keypoints_gt[row_gri] - keypoints_dt[col_gri], axis=-1)**2 / bboxs_gt[row_gri][:,None]
-                                #Y = dists[visibility_gt[row_gri] > 0]
-                                #X = scores_dt[col_gri][visibility_gt[row_gri] > 0]
-                                
-                                #plot_data[method]['scores'].extend(X)
-                                #plot_data[method]['L2'].extend(Y)
-                            
+                            row_gri, col_gri, costs_OKS = assignPeople(keypoints_gt, keypoints_dt, visibility_gt, bboxs_gt)                            
                             
                             if args.verbose:
                                 print(f'{dat = } {method = } {frame_id = } {costs_OKS = }')
@@ -303,19 +248,10 @@ if __name__ == '__main__':
                             print(e)
                             print(f'{dat = } {fl_name = } {method = } {frame_id = }')
                             raise e
-
-
-                
-                #for (method, values) in plot_data.items():
-                #    plt.semilogy(values['scores'], values['L2'], '.', label=method)
-                #plt.legend()
-                #plt.xlabel('Confidence score')
-                #plt.ylabel('L2^2/bbox')
-                #plt.title(f'{dat}_{fl_name}')
-                #plt.savefig(f'./figs/{dat}_{fl_name}.pdf')
-                #plt.close()
                 
 
     results_csv = pd.DataFrame.from_dict(method_results_dict)
-    if args.save:
-        results_csv.to_csv(os.path.join(pth, 'results/', 'results.csv'))
+    if not args.out is None:
+        results_csv.to_csv(args.out)
+    else:
+        print(results_csv.to_string())
